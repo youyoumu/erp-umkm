0d17419be1e976c5663df9cd63a8f3ece0ad7b99
youyoumu <19760884+youyoumu@users.noreply.github.com>
2024-09-02 15:50:52 +0700

    Install shadcn-svelte

--- app/frontend/pages/Dashboard/Index.svelte
+++ app/frontend/pages/Dashboard/Index.svelte
@@ -1,1 +1,7 @@
-<main class="container">testtt</main>
+<script>
+import { Button } from "$lib/components/ui/button"
+</script>
+
+<main class="container h-svh bg-slate-100">
+  <Button>Default</Button>
+</main>
