5853464e6de70bb51838d717e1c1c5b19a2d3ffe
youyoumu <19760884+youyoumu@users.noreply.github.com>
2024-09-08 14:36:41 +0700

    Add table to invoices index

--- app/frontend/pages/Invoice/Index.svelte
+++ app/frontend/pages/Invoice/Index.svelte
@@ -7,11 +7,52 @@ import { Link, inertia } from "@inertiajs/svelte"
 import Button from "$lib/components/ui/button/button.svelte"
 export let invoices
 export let flash
+import { onMount } from "svelte"
+import { createGrid } from "ag-grid-community"
+import "ag-grid-community/styles/ag-grid.css"
+import "ag-grid-community/styles/ag-theme-alpine.css"
+import { formatIDR, cn } from "$lib/utils"
+import dayjs from "dayjs"
+import Input from "$lib/components/ui/input/input.svelte"
+import { cellRendererFactory } from "$lib/cellRendererFactory"
+import InvoiceDetailLink from "./components/InvoiceDetailLink.svelte"
 
-const onDestroy = (e) => {
-  if (!confirm("Are you sure you want to delete this invoice?")) {
-    e.preventDefault()
-  }
+let gridContainer
+const columnDefs = [
+  {
+    field: "code",
+    headerName: "Kode Nota",
+    flex: 1,
+    cellRenderer: cellRendererFactory((c, p) => {
+      console.log(p)
+      new InvoiceDetailLink({
+        target: c.eGui,
+        props: {
+          invoice: p.data,
+        },
+      })
+    }),
+  },
+  { field: "date", headerName: "Tanggal", width: 115 },
+  { field: "address", headerName: "Alamat", width: 320 },
+]
+const gridOptions = {
+  columnDefs: columnDefs,
+  rowData: invoices.map((invoice) => {
+    return {
+      ...invoice,
+      date: dayjs(invoice.date).format("DD/MMM/YY"),
+    }
+  }),
+}
+
+let gridApi
+onMount(() => {
+  gridApi = createGrid(gridContainer, gridOptions)
+})
+
+function handleSearch(e) {
+  gridApi.setGridOption("quickFilterText", e.target.value)
 }
 </script>
 
@@ -31,13 +72,6 @@ const onDestroy = (e) => {
     <a href="/invoices/new" use:inertia><Button>Nota Baru</Button></a>
   </div>
 
-  <div class="min-w-full">
-    {#each invoices as invoice (invoice.id)}
-      <p>
-        <Link href={`/invoices/${invoice.id}`} class="text-blue-600 underline">
-          {invoice.code}
-        </Link>
-      </p>
-    {/each}
-  </div>
+  <Input on:input={handleSearch} placeholder="Cari Barang" class="mb-4" />
+  <div id="datagrid" class={cn("ag-theme-alpine h-[60svh] w-full")} bind:this={gridContainer}></div>
 </div>
---
+++ app/frontend/pages/Invoice/components/InvoiceDetailLink.svelte
@@ -0,0 +1,9 @@
+<script>
+import { ExternalLink } from "lucide-svelte"
+export let invoice
+</script>
+
+<a href="/invoices/{invoice.id}" class="flex size-full items-center gap-2" target="_blank">
+  <div>{invoice.code}</div>
+  <ExternalLink opacity="0.25" size="20" />
+</a>
