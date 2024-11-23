7d3185bb157d831fd4202b03c0ce40f1f4b1b953
youyoumu <19760884+youyoumu@users.noreply.github.com>
2024-11-23 10:07:44 +0700

    Validate Item model

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,11 +1,36 @@
 class Item < ApplicationRecord
-  attribute :quantity, :float, default: 0
-  attribute :quantity_unit, :string, default: ""
-  attribute :tag, :string, default: ""
+  attribute :name, :string, default: ""
+  validates :name,
+    presence: true,
+    length: {maximum: 255}
 
-  validates :name, presence: {message: "Nama barang harus diisi"}
-  validates :cost_price, numericality: {message: "Harga modal harus berupa angka"}
+  attribute :notes, :string, default: ""
+  validates :notes, length: {maximum: 65535}, allow_blank: true
+
   attribute :cost_price, :decimal, default: 0
-  validates :selling_price, numericality: {message: "Harga modal harus berupa angka"}
   attribute :selling_price, :decimal, default: 0
+  validates :cost_price, :selling_price,
+    numericality: {greater_than_or_equal_to: 0}
+
+  attribute :stock, :string, default: ""
+  validates :stock, length: {maximum: 255}, allow_blank: true
+
+  attribute :code, :string, default: ""
+  validates :code, allow_blank: true, length: {maximum: 255}
+
+  attribute :category, :string, default: ""
+  validates :category, length: {maximum: 255}, allow_blank: true
+
+  attribute :quantity, :float, default: 0
+  validates :quantity,
+    numericality: {greater_than_or_equal_to: 0}
+
+  attribute :quantity_unit, :string, default: ""
+  validates :quantity_unit, length: {maximum: 255}, allow_blank: true
+
+  attribute :tag, :string, default: ""
+  validates :tag, length: {maximum: 255}, allow_blank: true
+
+  attribute :is_snapshot, :boolean, default: false
+  validates :is_snapshot, inclusion: {in: [true, false]}
 end
