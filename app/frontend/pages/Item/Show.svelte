5fc8de4754bd1a95c7e6ce0851049e60ae8a7117
youyoumu <19760884+youyoumu@users.noreply.github.com>
2024-11-30 14:05:23 +0700

    Display total sold

--- app/frontend/pages/Item/Show.svelte
+++ app/frontend/pages/Item/Show.svelte
@@ -17,8 +17,6 @@
     sold_snapshots_grouped_by_quantity_unit: Record<string, Item[]>
   } = $props()
 
-  $inspect(sold_snapshots_grouped_by_quantity_unit)
-
   const onDestroy = () => {
     router.delete(`/items/${item.id}`)
   }
@@ -27,6 +25,13 @@
 <svelte:head>
   <title>Barang #{item.id}</title>
 </svelte:head>
+
+{#snippet soldPerQuantityUnit(quantity_unit: string, items: Item[])}
+  <div>
+    {items.reduce((total, item) => total + item.quantity, 0)}
+    {quantity_unit}
+  </div>
+{/snippet}
 
 <div class="mx-auto max-w-screen-sm p-8">
   <div class="mx-auto">
@@ -40,7 +45,7 @@
 
     <h1 class="mb-4 text-4xl font-bold">Barang #{item.id}</h1>
 
-    <div class="flex flex-col gap-1">
+    <div class="flex flex-col gap-1 pb-4">
       <ItemDetail label="Nama" value={item.name} />
       <ItemDetail label="Harga Modal" value={item.cost_price} />
       <ItemDetail label="Harga Jual" value={item.selling_price} />
@@ -50,14 +55,19 @@
       <ItemDetail label="Kategori" value={item.category} />
       <ItemDetail label="Tag" value={item.tag} />
     </div>
-    <div class="my-4">
+    <div class="pb-4">
       <div class="mb-2 font-bold">Catatan:</div>
       <div class="min-h-32 rounded-sm border border-slate-300 p-2">
         {item.notes}
       </div>
     </div>
 
-    <div class="flex gap-2">
+    <div class="font-bold">Total Terjual:</div>
+    {#each Object.entries(sold_snapshots_grouped_by_quantity_unit) as [quantity_unit, items]}
+      {@render soldPerQuantityUnit(quantity_unit, items)}
+    {/each}
+
+    <div class="flex gap-2 pt-4">
       <a href={`/items/${item.id}/edit`} use:inertia
         ><Button variant="secondary">Edit</Button></a
       >
