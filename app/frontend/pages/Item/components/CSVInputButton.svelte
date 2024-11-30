b697d4f835e78fd2b739708aafcc8314a6314e39
youyoumu <19760884+youyoumu@users.noreply.github.com>
2024-11-30 15:26:14 +0700

    Setup csv parser

--- app/frontend/pages/Item/components/CSVInputButton.svelte
+++ app/frontend/pages/Item/components/CSVInputButton.svelte
@@ -1,5 +1,27 @@
 <script lang="ts">
+  import { parse } from 'csv-parse/browser/esm'
   import { Table } from 'lucide-svelte'
+
+  function handleFileInput(event: Event) {
+    const file = (event.target as HTMLInputElement).files?.[0]
+    if (!file) return
+    const reader = new FileReader()
+    reader.readAsText(file)
+    reader.onload = (e) => {
+      const csv = e.target?.result as string
+      parse(
+        csv,
+        {
+          columns: true,
+          skip_empty_lines: true,
+          from_line: 2,
+        },
+        (err, data) => {
+          console.log(data)
+        }
+      )
+    }
+  }
 </script>
 
 <label
@@ -9,4 +31,10 @@
   <Table size="20" />
   Import CSV
 </label>
-<input type="file" id="import-csv" accept=".csv" class="hidden" />
+<input
+  type="file"
+  id="import-csv"
+  accept=".csv"
+  class="hidden"
+  onchange={handleFileInput}
+/>
