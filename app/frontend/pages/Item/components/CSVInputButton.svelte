fa9d3b6ce07fa15ddec231c51a8c6114c58c36f3
youyoumu <19760884+youyoumu@users.noreply.github.com>
2024-11-30 18:44:39 +0700

    Add dialog when selecting file

--- app/frontend/pages/Item/components/CSVInputButton.svelte
+++ app/frontend/pages/Item/components/CSVInputButton.svelte
@@ -2,7 +2,16 @@
   import { parse } from 'csv-parse/browser/esm'
   import { Table } from 'lucide-svelte'
 
+  import * as AlertDialog from '$lib/components/ui/alert-dialog/index.js'
+  import Button from '$lib/components/ui/button/button.svelte'
+
   import importCSVItems from '../utils/importCSVItems'
+
+  let isOpen = $state(false)
+  let alertTitle = $state('')
+  let alertDescription = $state('')
+  let csvData = $state<Record<string, string>[]>([])
+  let isError = $state(false)
 
   function handleFileInput(event: Event) {
     const file = (event.target as HTMLInputElement).files?.[0]
@@ -19,12 +28,25 @@
           from_line: 2,
         },
         (err, data) => {
-          if (err) return console.log(err)
-          importCSVItems(data)
+          if (err) {
+            alertTitle = 'Tidak dapat membaca CSV'
+            alertDescription = err.message.slice(0, 100)
+            isError = true
+            isOpen = true
+            return
+          }
+
+          alertTitle = 'Import CSV?'
+          alertDescription = ''
+          csvData = data
+          isError = false
+          isOpen = true
         }
       )
     }
   }
+
+  $inspect(isOpen)
 </script>
 
 <label
@@ -41,3 +63,38 @@
   class="hidden"
   onchange={handleFileInput}
 />
+<AlertDialog.Root
+  open={isOpen}
+  onOpenChange={(open) => {
+    isOpen = open
+    if (!open) csvData = []
+  }}
+>
+  <AlertDialog.Content>
+    <AlertDialog.Header>
+      <AlertDialog.Title>{alertTitle}</AlertDialog.Title>
+      <AlertDialog.Description>
+        <div>{alertDescription}</div>
+        <div class="flex flex-col gap-2">
+          {#each csvData.slice(0, 3) as row}
+            <div>
+              {#each Object.entries(row) as [key, value]}
+                {key}: {value},{' '}
+              {/each}
+            </div>
+          {/each}
+        </div>
+      </AlertDialog.Description>
+    </AlertDialog.Header>
+    <AlertDialog.Footer>
+      {#if isError}
+        <AlertDialog.Cancel class="bg-primary text-primary-foreground">
+          Ok
+        </AlertDialog.Cancel>
+      {:else}
+        <AlertDialog.Cancel>Batal</AlertDialog.Cancel>
+        <AlertDialog.Action>Import</AlertDialog.Action>
+      {/if}
+    </AlertDialog.Footer>
+  </AlertDialog.Content>
+</AlertDialog.Root>
