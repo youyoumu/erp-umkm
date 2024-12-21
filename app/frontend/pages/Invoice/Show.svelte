7b28df14f5467a81eadfac3e2bdb9b107e0d8b2e
youyoumu <19760884+youyoumu@users.noreply.github.com>
2024-12-21 22:43:01 +0700

    Add tooltip to print icon

--- app/frontend/pages/Invoice/Show.svelte
+++ app/frontend/pages/Invoice/Show.svelte
@@ -4,6 +4,7 @@
 
   import * as AlertDialog from '$lib/components/ui/alert-dialog'
   import Button from '$lib/components/ui/button/button.svelte'
+  import * as Tooltip from '$lib/components/ui/tooltip'
   import type { Invoice } from '$types/typelizer'
 
   import InvoiceComponent from './Invoice.svelte'
@@ -34,44 +35,61 @@
     </p>
   {/if}
 
-  <a
-    href={`/invoices/${invoice.id}/print`}
-    target="_blank"
-    class="absolute right-12 top-8 cursor-pointer rounded-full border border-slate-300 bg-green-200 p-2 shadow-sm"
-    ><Printer /></a
-  >
-  <a
-    href={`/invoices/${invoice.id}/display_invoice`}
-    target="_blank"
-    class="absolute right-0 top-8 cursor-pointer rounded-full border border-slate-300 bg-blue-200 p-2 shadow-sm"
-    ><Printer /></a
-  >
+  <div class="mb-4 flex w-full items-center gap-4 justify-between">
+    <div class="flex gap-4 items-center">
+      <h1 class="text-4xl font-bold">Nota #{invoice.id}</h1>
+      <a href={`/invoices/${invoice.id}/edit`} use:inertia>
+        <Button variant="secondary">Edit Nota</Button>
+      </a>
+      <AlertDialog.Root>
+        <AlertDialog.Trigger
+          ><Button variant="destructive">Hapus Nota</Button
+          ></AlertDialog.Trigger
+        >
+        <AlertDialog.Content>
+          <AlertDialog.Header>
+            <AlertDialog.Title
+              >Apakah kamu yakin ingin menghapus nota ini?</AlertDialog.Title
+            >
+            <AlertDialog.Description></AlertDialog.Description>
+          </AlertDialog.Header>
+          <AlertDialog.Footer>
+            <AlertDialog.Cancel>Batal</AlertDialog.Cancel>
+            <AlertDialog.Action
+              onclick={onDestroy}
+              class="bg-red-600 hover:bg-red-700">Hapus</AlertDialog.Action
+            >
+          </AlertDialog.Footer>
+        </AlertDialog.Content>
+      </AlertDialog.Root>
+    </div>
 
-  <div class="mb-4 flex w-full items-center gap-4">
-    <h1 class="text-4xl font-bold">Nota #{invoice.id}</h1>
-    <a href={`/invoices/${invoice.id}/edit`} use:inertia>
-      <Button variant="secondary">Edit Nota</Button>
-    </a>
-    <AlertDialog.Root>
-      <AlertDialog.Trigger
-        ><Button variant="destructive">Hapus Nota</Button></AlertDialog.Trigger
-      >
-      <AlertDialog.Content>
-        <AlertDialog.Header>
-          <AlertDialog.Title
-            >Apakah kamu yakin ingin menghapus nota ini?</AlertDialog.Title
-          >
-          <AlertDialog.Description></AlertDialog.Description>
-        </AlertDialog.Header>
-        <AlertDialog.Footer>
-          <AlertDialog.Cancel>Batal</AlertDialog.Cancel>
-          <AlertDialog.Action
-            onclick={onDestroy}
-            class="bg-red-600 hover:bg-red-700">Hapus</AlertDialog.Action
-          >
-        </AlertDialog.Footer>
-      </AlertDialog.Content>
-    </AlertDialog.Root>
+    <div class="flex gap-4 items-center">
+      <Tooltip.Provider delayDuration={100}>
+        <Tooltip.Root>
+          <Tooltip.Trigger>
+            <a
+              href={`/invoices/${invoice.id}/print`}
+              target="_blank"
+              class="cursor-pointer rounded-full border border-slate-300 bg-green-200 p-2 shadow-sm block"
+              ><Printer /></a
+            >
+          </Tooltip.Trigger>
+          <Tooltip.Content>HTML</Tooltip.Content>
+        </Tooltip.Root>
+        <Tooltip.Root>
+          <Tooltip.Trigger>
+            <a
+              href={`/invoices/${invoice.id}/display_invoice`}
+              target="_blank"
+              class="cursor-pointer rounded-full border border-slate-300 bg-blue-200 p-2 shadow-sm block"
+              ><Printer /></a
+            >
+          </Tooltip.Trigger>
+          <Tooltip.Content>PDF</Tooltip.Content>
+        </Tooltip.Root>
+      </Tooltip.Provider>
+    </div>
   </div>
   <InvoiceComponent {invoice} />
 </div>
