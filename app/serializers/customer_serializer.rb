57ae28e76b91d73768d60906f94582f3396634ba
youyoumu <19760884+youyoumu@users.noreply.github.com>
2025-01-12 17:03:57 +0700

    Rename serializer to resource

--- app/serializers/customer_serializer.rb
+++ app/serializers/customer_serializer.rb
@@ -1,5 +1,5 @@
 class CustomerSerializer < ApplicationSerializer
   attributes :id, :name, :notes, :address, :contact, :updated_at
-  has_many :invoices, serializer: InvoiceSerializer
-  has_many :today_invoices, serializer: InvoiceSerializer
+  has_many :invoices, resource: InvoiceSerializer
+  has_many :today_invoices, resource: InvoiceSerializer
 end
--- app/serializers/invoice_serializer.rb
+++ app/serializers/invoice_serializer.rb
@@ -2,6 +2,6 @@ class InvoiceSerializer < ApplicationSerializer
   attributes :id, :date, :code, :address
   typelize address: :string
 
-  has_one :customer, serializer: CustomerSerializer
-  has_many :items, serializer: ItemSerializer
+  has_one :customer, resource: CustomerSerializer
+  has_many :items, resource: ItemSerializer
 end
